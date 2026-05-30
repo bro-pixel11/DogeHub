35761586ead9cecba02673d52a2c2b740306b6ec
bro-pixel11 <serafimavsalumov@gmail.com>
2026-05-30 12:42:41 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,11 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title = "DogeHub",
-    Text = "Loader works",
-    Duration = 5
-}) 
+local WORD_BOMB = 2653064683
+
+if game.PlaceId == WORD_BOMB then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bro-pixel11/DogeHub/main/games/wordbomb.lua"))()
+else
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "DogeHub",
+        Text = "Unsupported game",
+        Duration = 5
+    })
+end
