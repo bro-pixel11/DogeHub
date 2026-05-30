38030793820aad30ac1def366730946c69dbe078
bro-pixel11 <serafimavsalumov@gmail.com>
2026-05-30 13:37:21 +0300

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -4,5 +4,7 @@ loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Lib-18698")
 lib.makelib("DogeHub")
 
 local home = lib.maketab("Home")
-
 lib.makelabel("DogeHub Loaded", home)
+
+local wordbomb = lib.maketab("WordBomb")
+lib.makelabel("WordBomb Module Loaded", wordbomb)
