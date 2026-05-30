197523cc3d11eac265eb5ed3857c8c679d991ced
bro-pixel11 <serafimavsalumov@gmail.com>
2026-05-30 12:18:08 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,9 @@
-print("loader")
+local place = game.PlaceId
+
+local WORD_BOMB = 2653064683
+
+if place == WORD_BOMB then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bro-pixel11/DogeHub/main/games/wordbomb.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bro-pixel11/DogeHub/main/games/universal.lua"))()
+end
