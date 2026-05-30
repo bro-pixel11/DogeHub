225f2ffdfb6cb91132db17eac269b25d094e70ea
bro-pixel11 <serafimavsalumov@gmail.com>
2026-05-30 13:24:05 +0300

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -1,1 +1,10 @@
+local lib =
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Lib-18698"))()
+
+lib.makelib("DogeHub")
+
+local home = lib.maketab("Home")
+
+lib.makelabel("DogeHub Loaded", home)
+
 print("DogeHub UI loaded")
